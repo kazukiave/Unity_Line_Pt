47112aa3e35c6a15581158a3f49b2c7fffa8d6c9
kazukiave <57382140+kazukiave@users.noreply.github.com>
2020-02-05 17:24:51 +0900

    Add files via upload

---
+++ DrawLine.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+
+    
+    Vector3 hitDown = Vector3.zero;
+    Vector3 hitUp = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.SphereCast(ray, 1f, out hit, Mathf.Infinity))
+            {
+                hitDown = hit.transform.position;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.SphereCast(ray, 1f, out hit, Mathf.Infinity))
+            {
+                hitUp = hit.transform.position;
+            }
+
+            if (hitUp != Vector3.zero && hitDown != Vector3.zero)
+            {
+                var line = new GameObject("line");
+                var lineRender = line.AddComponent<LineRenderer>();
+                lineRender.SetPosition(0, hitDown);
+                lineRender.SetPosition(1, hitUp);
+
+                hitDown = Vector3.zero;
+                hitUp = Vector3.zero;
+            }
+        }
+
+
+    }
+}
---
+++ PointPlotter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointPlotter : MonoBehaviour
+{
+    public GameObject point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+            GameObject.Instantiate(point, pos, Quaternion.identity);
+        }
+    }
+}
